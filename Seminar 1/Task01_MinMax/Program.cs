40a09a8115be302a06627d78681c6f4a57615d8a
avraamovolono <avraamovolono@yandex.ru>
2022-10-03 23:38:58 +0300

    Add Task 1 Min Max project

--- Seminar 1/Task01_MinMax/Program.cs
+++ Seminar 1/Task01_MinMax/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+
+// Блок ввода:
+
+Console.WriteLine("Давайте сравним два числа!");
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе чисто: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// Сравниваем числа:
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine($"Число {firstNumber} больше числа {secondNumber}.");
+}
+else if (secondNumber > firstNumber)
+{
+    Console.WriteLine($"Число {secondNumber} больше числа {firstNumber}.");
+}
+else
+{
+    Console.WriteLine("Числа равны! Что тут сравнивать?");
+}
