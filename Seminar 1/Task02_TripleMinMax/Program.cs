40e9c4991750c56945c1922fcd9d8b8d9fa96fe4
avraamovolono <avraamovolono@yandex.ru>
2022-10-04 00:09:32 +0300

    Add Task 2 Triple Min Max project

---
+++ Seminar 1/Task02_TripleMinMax/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+
+int [] arr = new int[3];
+int i = 0, max = 0;
+
+Console.WriteLine("Введите три числа для сравнения:");
+
+while (i < arr.Length)
+{
+    arr[i] =  Convert.ToInt32(Console.ReadLine());
+    i++;
+}
+
+for (i = 0; i < arr.Length; i++)
+{
+    if(arr[i] > max)
+    {
+        max = arr[i];
+    }
+}
+
+Console.WriteLine($"Максимальное число из трех: {max}");
