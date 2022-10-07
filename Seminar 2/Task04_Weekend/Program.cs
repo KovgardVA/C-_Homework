90984a9377884eb89ac86f021088c711fbd3deaa
avraamovolono <avraamovolono@yandex.ru>
2022-10-07 16:41:47 +0300

    Add Task 04 Weekend project

---
+++ Seminar 2/Task04_Weekend/Program.cs
@@ -0,0 +1,22 @@
+﻿int Prompt(string massage)
+{
+    Console.WriteLine(massage);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+Console.WriteLine("Давайте найдем выходные дни!");
+int weekend = Prompt("Введите число, обозночающее день недели (от 1 до 7): ");
+
+if (weekend > 0 & weekend < 6)
+{
+    Console.WriteLine("Это будний день :(");
+}
+else if (weekend > 5 & weekend < 8)
+{
+    Console.WriteLine("Ура! Это выходной :)");
+}
+else
+{
+    Console.WriteLine("Ой! Вы ввели цифру не обозначающую день недели.");
+}
