4bf8678f67f820eaafaffa9ed85a5c0ed6c4451d
avraamovolono <avraamovolono@yandex.ru>
2022-10-07 16:18:37 +0300

    Add Task 02 Delete Second project

---
+++ Seminar 2/Task02_DeleteSecond/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число
+// и удаляет вторую цифру этого числа.
+
+Console.WriteLine("Эта программа удалит вторую цифру из случайного трехзначного числа!");
+int randomNumber = new Random().Next(100,1000);
+Console.WriteLine("Ваше число: " + randomNumber);
+
+int number = (randomNumber / 100) * 10 + randomNumber % 10;
+
+Console.WriteLine($"Число без второй цифры: {number}");
