12c8f541313ba7cbdbcad72c25cbe61613b2c72e
avraamovolono <avraamovolono@yandex.ru>
2022-10-13 16:47:40 +0300

    Add validate function to Task 01 Show Second project.

--- Seminar 2/Task01_ShowSecond/Program.cs
+++ Seminar 2/Task01_ShowSecond/Program.cs
@@ -11,24 +11,30 @@ int Module(int module)
     return module;
 }
 
-int Prompt(string massage)
+int Prompt(string message)
 {
-    Console.WriteLine(massage);
+    Console.WriteLine(message);
     int number = Convert.ToInt32(Console.ReadLine());
     return number;
+}
+
+bool ValidateNumber(int number)
+{
+    if (number > 99 & number < 1000)
+    {
+        return true;
+    }
+    Console.WriteLine("Это не трехзначное число!");
+    return false;
 }
 
 Console.WriteLine("Давайте найдем вторую цифру трехзначного числа!");
 int number = Prompt("Введите число: ");
 
-int secondNumber = 0;
 number = Module(number); // Числа могут быть не только положительными, но там тоже есть вторая цифра.
-if (number > 99 & number < 1000)
+if (ValidateNumber(number))
 {
+    int secondNumber = 0;
     secondNumber = (number / 10) % 10; // Отрезаем третью цифру (/ 10) и забираем вторую (% 10).
     Console.WriteLine($"Вторая цифра этого числа: {secondNumber}");
-}
-else
-{
-    Console.WriteLine("Это не трехзначное число!");
 }
