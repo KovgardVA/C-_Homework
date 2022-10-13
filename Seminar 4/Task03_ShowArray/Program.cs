f5479b4aad9900513247fa4a08ac9a552a99b1b1
avraamovolono <avraamovolono@yandex.ru>
2022-10-14 00:53:12 +0300

    Add Task 03 Show Array project

---
+++ Seminar 4/Task03_ShowArray/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая задаёт массив из 8 случайных элементов
+// и выводит их на экран.
+
+int[] FillArray(int length)
+{
+    int[] collection = new int[length];
+    for(int i = 0; i < collection.Length; i++) collection[i] = new Random().Next(-100,100);
+    return collection;
+}
+
+void PrintArrya(int[] col)
+{
+    int count = col.Length;
+    for (int position = 0; position < count; position++) Console.Write(col[position] + " ");
+}
+
+PrintArrya(FillArray(8));
