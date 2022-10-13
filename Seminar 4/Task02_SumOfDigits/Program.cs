958ec75fa62d21b18f8564f2dcbe3a0eca237ad9
avraamovolono <avraamovolono@yandex.ru>
2022-10-14 00:48:54 +0300

    Add Task 02 Sum Of Digits project

---
+++ Seminar 4/Task02_SumOfDigits/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int Module(int moduleNumber)
+{
+    if (moduleNumber < 0)
+    {
+        moduleNumber = -moduleNumber;
+    }
+    return moduleNumber;
+}
+
+int SumOfDigits (int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum = sum + number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+int num = Prompt("Введите натуральное число: ");
+Console.Write($"Сумма цифр в числе: {SumOfDigits(Module(num))}");
