33329b64d53083a4fa027a1be9668bf02ce27954
avraamovolono <avraamovolono@yandex.ru>
2022-10-20 22:57:52 +0300

    Add Task 01 Count Of Positive project.

---
+++ Seminar 6/Task01_CountOfPositive/Program.cs
@@ -0,0 +1,25 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// Метод создания и заполнения массива с консоли с заданным количеством элементов.
+int[] FillNewArray (int M)
+{
+    Console.WriteLine($"Введите {M} чисел:");
+    int[] array = new int[M];
+    for (int i = 0; i < M; i++) array[i] = Convert.ToInt32(Console.ReadLine());
+    return array;
+}
+
+// Метод подсчета положительных чисел счетчиком.
+int CountOfPositive (int[] collection)
+{
+    int count = 0;
+    for (int index = 0; index < collection.Length; index++)
+    {
+        if (collection[index] > 0) count++;
+    }
+    return count;
+}
+
+int[] myArray = FillNewArray(5);
+Console.WriteLine($"Количество положительных чисел: {CountOfPositive(myArray)}");
