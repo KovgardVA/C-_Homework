c084f89accf930d4d79d4268929d29fe69817a96
AvraamOvolono <avraamovolono@yandex.ru>
2022-10-28 12:54:01 +0300

    Add Task 01 Double Matrix project.

---
+++ Seminar 7/Task01_DoubleMatrix/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] CreateArray(int lenRow, int lenColumn)
+{
+    double[,] array = new double[lenRow, lenColumn];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(-5, 5) + random.NextDouble();
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]:f1}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] myArray = CreateArray(3, 4);
+PrintArray(myArray);
