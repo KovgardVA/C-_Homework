996e820809d7f7dd59f6006bf21cba64d47b7d66
AvraamOvolono <avraamovolono@yandex.ru>
2022-11-17 22:11:51 +0300

    Add Task 01 Even Num project.

---
+++ Seminar 9/Task01_EvenNum/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все
+// чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+bool Validate(int from, int to)
+{
+    if (from == to || from > to) return false;
+    return true;
+}
+
+void PrintEvenNumbers(int from, int to)
+{
+    if (to % 2 != 0) to -= 1;
+    if (to < from) return;
+    PrintEvenNumbers(from, to - 2);
+    Console.Write(to + " ");
+}
+
+int firstNumber = Prompt("Введите число от: ");
+int secondNumber = Prompt("Введите число до: ");
+if(Validate(firstNumber,secondNumber)) PrintEvenNumbers(firstNumber, secondNumber);
+else Console.WriteLine("Пожалуйста, введите другие числа!");
