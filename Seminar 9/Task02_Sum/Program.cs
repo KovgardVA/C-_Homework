f59009060696fa3a546e226d2925a6c4db7a8ca9
AvraamOvolono <avraamovolono@yandex.ru>
2022-11-17 22:24:33 +0300

    Add Task 02 Sum project.

---
+++ Seminar 9/Task02_Sum/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+bool Validate(int from, int to)
+{
+    if (from == to || from > to) return false;
+    return true;
+}
+
+int Sum(int from, int to)
+{
+    if (from > to) return 0;
+    return from + Sum(from + 1, to);
+}
+
+int firstNumber = Prompt("Введите число от: ");
+int secondNumber = Prompt("Введите число до: ");
+if(Validate(firstNumber,secondNumber))
+{
+    Console.WriteLine($"Сумма всех натуральных чисел равна {Sum(firstNumber, secondNumber)}");
+}
+else Console.WriteLine("Пожалуйста, введите другие числа!");
